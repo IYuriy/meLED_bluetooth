284f0f1a7dafe8bccceb87431e8e694e5fc3af8d
IYuriy <iuriy.kochura@gmail.com>
2019-07-13 21:38:11 +0300

    first commit

---
+++ meLED.ino
@@ -0,0 +1,20 @@
+char data = ' ';
+void setup() {
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+
+}
+
+void loop() {
+  if(Serial.available() > 0)
+  data = Serial.read();
+  Serial.println(data);
+  if(data == 'n')
+  digitalWrite(13, HIGH);
+  else if(data == 'f')
+  digitalWrite(13, LOW);
+
+ /* while(Serial.available())
+  Serial.read();
+  */
+}
